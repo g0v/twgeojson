93c92ac477de428dacb96a2a1357d54bc2b3ec70
Kirby T. Wu <tkirby@gmail.com>
2013-01-23 20:39:24 +0800

    add cartogram transition

--- topojson.js
+++ topojson.js
@@ -1,17 +1,23 @@
 (function(){
   $(document).ready(function(){
-    var svg, layer, states, proj, carto;
+    var cc, svg, layer, states, proj, carto;
+    cc = 1;
     svg = d3.select('#map');
     layer = svg.append('g').attr('id', 'layer');
     states = layer.append('g', 'id', 'states').selectAll('path');
     proj = d3.geo.mercator().scale(50000).translate([-16500, 3650]);
-    carto = d3.cartogram().projection(proj);
+    carto = d3.cartogram().projection(proj).value(function(){
+      return Math.random() * 100;
+    });
     return d3.json('twCounty1982.topojson', function(topo){
       var geometries, features, path;
       geometries = topo.objects.twCounty1982.geometries;
       features = carto.features(topo, geometries);
       path = d3.geo.path().projection(proj);
-      return states = states.data(features).enter().append('path').attr('class', 'state').attr('fill', '#fafafa').attr('stroke-width', '1px').attr('stroke', '#999').attr('d', path);
+      states = states.data(features).enter().append('path').attr('class', 'state').attr('fill', '#fafafa').attr('stroke-width', '1px').attr('stroke', '#999').attr('d', path);
+      features = carto(topo, geometries).features;
+      states.data(features);
+      return states.transition().duration(1000).attr('d', carto.path);
     });
   });
 }).call(this);
--- topojson.ls
+++ topojson.ls
@@ -1,10 +1,11 @@
 <- $ document .ready
 
+cc=1
 svg = d3.select \#map
 layer = svg.append \g .attr \id \layer
 states = layer.append \g \id \states .selectAll \path
 proj = d3.geo.mercator!.scale 50000 .translate [-16500, 3650]
-carto = d3.cartogram!.projection proj 
+carto = d3.cartogram!.projection proj .value -> Math.random! * 100
 
 d3.json \twCounty1982.topojson, (topo) ->
   geometries = topo.objects.twCounty1982.geometries
@@ -15,4 +16,8 @@ d3.json \twCounty1982.topojson, (topo) ->
     .attr \fill \#fafafa
     .attr \stroke-width \1px
     .attr \stroke \#999
-    .attr \d path
+    .attr \d path 
+  features = carto topo, geometries .features
+  states.data(features)
+  states.transition! .duration 1000 .attr \d carto.path
+    #.attr \transform "translate(-2000 -400) scale(20)"
