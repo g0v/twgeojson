bc07fe3163feda1da631ddd18984b8b836c912ba
Audrey Tang <audreyt@audreyt.org>
2013-12-14 23:59:42 +0800

    * Add Firefox OS

--- cordova/res/icons/convert.sh
+++ cordova/res/icons/convert.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+mkdir fxos || true
+convert my-hires-icon.png -resize 16x16 fxos/icon-16.png
+convert my-hires-icon.png -resize 30x30 fxos/icon-30.png
+convert my-hires-icon.png -resize 32x32 fxos/icon-32.png
+convert my-hires-icon.png -resize 48x48 fxos/icon-48.png
+convert my-hires-icon.png -resize 60x60 fxos/icon-60.png
+convert my-hires-icon.png -resize 90x90 fxos/icon-90.png
+convert my-hires-icon.png -resize 120x120 fxos/icon-120.png
+convert my-hires-icon.png -resize 128x128 fxos/icon-128.png
+convert my-hires-icon.png -resize 256x256 fxos/icon-256.png
+convert my-hires-icon.png -resize 1000x1000 fxos/icon-1000.png
+convert my-hires-icon.png -resize 1024x1024 fxos/icon-1024.png
 
 mkdir android || true
 mkdir android/drawable || true
